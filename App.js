94efbea1c94e276631ccea1d2339beab622f9ec9
RashanWeerasinghe <malithrashan03@gmail.com>
2021-06-16 16:30:18 +0530

    test

--- App.js
+++ App.js
@@ -81,7 +81,7 @@ const App: () => Node = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Read docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
         </View>
