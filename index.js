384506342566bf83577836c5df30e2045a2706e6
KalanaRavishanka <ultimatekaklana@gmail.com>
2021-07-07 21:00:51 +0530

    login logout screens

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+
+
 
 AppRegistry.registerComponent(appName, () => App);
