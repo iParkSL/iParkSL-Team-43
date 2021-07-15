0651487ab6c086e78c15a70ccda84840d55e2f59
tharusalgadu <tharukasalgadu@gmaul.com>
2021-07-15 23:14:55 +0530

    Created edit park

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './screens/customerReview';
+import App from './screens/owner/functions/editpark';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
