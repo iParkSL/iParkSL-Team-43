869afbcccb4fcf212fc9f8e928cf970615111bb9
RashanWeerasinghe <malithrashan03@gmail.com>
2021-08-21 04:35:14 +0530

    update parking slots

--- OwnerApp/OwnerAppUI/screens/owner/functions/SlotUpdate.js
+++ OwnerApp/OwnerAppUI/screens/owner/functions/SlotUpdate.js
@@ -1,14 +1,95 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text} from 'react-native';
+import { Alert } from 'react-native';
+import {View, Text,StyleSheet,TouchableOpacity} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const SlotUP = ({route}) => {
   const data = route.params?.data;
+  const pid=118;
+const slotUpdate =()=>{
+  fetch('http://localhost:8080/SlotUpdate',{
+    method:'post',
+    headers:{
+      'Content-Type':'application/json',
+
+    },
+    body:JSON.stringify({
+      pid,
+    }),
+  }).then(res=>res.json()).catch(err=>{
+    console.log(err);
+  })
+  
+};
+
+
+
+
+
+
+
   return (
     <View>
-      <View><Text>{data}</Text></View>
+      <View><Text>{data}</Text>
+      <View style={styles.button}>
+          <TouchableOpacity style={styles.signIn} onPress={() => slotUpdate()
+         }>
+            <LinearGradient
+              colors={['#FDC73E', '#ffb907']}
+              style={[styles.signIn]}>
+              <Text
+                style={[
+                  styles.textSign,
+                  {
+                    color: '#000000',
+                  },
+                ]}>
+                Update slots
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      
+      
+      
+      </View>
     </View>
   );
 };
+
+
+const styles = StyleSheet.create({
+  
+  
+  button: {
+    marginTop: 15,
+    flexDirection: 'row',
+    // marginLeft:
+  },
+  signIn: {
+    width: '65%',
+    height: 50,
+    justifyContent: 'center',
+    // alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 16,
+    alignSelf: 'center',
+    fontWeight: '600',
+  },
+  
+});
+
+
+
+
+
+
+
+
+
+
 
 export default SlotUP;
--- OwnerApp/OwnerAppUI/screens/owner/functions/scan.js
+++ OwnerApp/OwnerAppUI/screens/owner/functions/scan.js
@@ -1,99 +1,3 @@
-// import React from 'react';
-// import {View, Text, Image, TouchableOpacity} from 'react-native';
-// import {RNCamera} from 'react-native-camera';
-// import {COLORS, SIZES, icons} from '../../../constants';
-// const Scan = ({navigation}) => {
-//   function Header() {
-//     return (
-//       <View
-//         style={{
-//           flexDirection: 'row',
-//           marginTop: 30,
-//           paddingHorizontal: 30,
-//         }}>
-//         <TouchableOpacity
-//           style={{
-//             flexDirection: 'row',
-//             alignItems: 'center',
-//             justifyContent: 'center',
-//           }}
-//           onPress={() => navigation.navigate('Home')}>
-//           <Image
-//             source={icons.cancel}
-//             style={{
-//               height: 40,
-//               width: 40,
-//               tintColor: COLORS.orange,
-//             }}
-//           />
-//         </TouchableOpacity>
-//       </View>
-//     );
-//   }
-//   function ScanQR() {
-//     return (
-//       <View
-//         style={{
-//           position: 'absolute',
-//           bottom: 0,
-//           left: 0,
-//           right: 0,
-//           height: 200,
-//           padding: 50,
-//           borderTopLeftRadius: SIZES.radius,
-//           borderTopRightRadius: SIZES.radius,
-//           backgroundColor: COLORS.white,
-//         }}>
-//         <View>
-//           <TouchableOpacity
-//             style={{
-//               alignItems: 'center',
-//               backgroundColor: COLORS.orange,
-//               padding: 10,
-//             }}>
-//             <Text style={{fontWeight: 'bold'}}>Scan QR Code</Text>
-//           </TouchableOpacity>
-//         </View>
-//       </View>
-//     );
-//   }
-//   function Focus() {
-//     return (
-//       <View
-//         style={{
-//           flex: 1,
-//           alignItems: 'center',
-//           justifyContent: 'center',
-//         }}>
-//         <Image
-//           source={icons.Focus}
-//           resizeMode="stretch"
-//           style={{
-//             marginTop: '-55%',
-//             width: 200,
-//             height: 300,
-//           }}
-//         />
-//       </View>
-//     );
-//   }
-
-//   return (
-//     <View style={{flex: 1, borderBottomColor: COLORS.orange}}>
-//       <RNCamera
-//         style={{flex: 1}}
-//         captureAudio={false}
-//         type={RNCamera.Constants.Type.back}
-//         flashMode={RNCamera.Constants.FlashMode.off}>
-//         {Header()}
-//         {ScanQR()}
-//         {Focus()}
-//       </RNCamera>
-//     </View>
-//   );
-// };
-// export default Scan;
-
 import React from 'react';
 import {
   View,
@@ -199,20 +103,6 @@ const Scan = ({navigation}) => {
   }
 
   return (
-    // <View style={{flex: 1, borderBottomColor: COLORS.orange}}>
-    //   <RNCamera
-    //     style={{flex: 1}}
-    //     captureAudio={false}
-    //     type={RNCamera.Constants.Type.back}
-    //     flashMode={RNCamera.Constants.FlashMode.off}>
-    //     {Header()}
-    //     {ScanQR()}
-    //     {Focus()}
-    //   </RNCamera>
-
-    // </View>
-    // <SafeAreaView style={{flex: 1}}>
-
     <QRCodeScanner
       style={{flex: 1}}
       onRead={onRead}
@@ -222,7 +112,7 @@ const Scan = ({navigation}) => {
         </Text>
       }
       bottomContent={
-        <View  style={styles.scanCardView}>
+        <View style={styles.scanCardView}>
           <Image
             source={icons.Focus}
             resizeMode="stretch"
@@ -230,13 +120,12 @@ const Scan = ({navigation}) => {
               marginTop: '-120%',
               width: 200,
               height: 300,
-              
             }}></Image>
           <ImageBackground style={styles.bottomContent}>
             <TouchableOpacity style={styles.buttonScan2}>
               <Image
                 source={icons.scanner}
-                style={{ marginTop: '50%',height: 75, width: 75}}></Image>
+                style={{marginTop: '50%', height: 75, width: 75}}></Image>
             </TouchableOpacity>
           </ImageBackground>
         </View>
@@ -293,5 +182,5 @@ const styles = StyleSheet.create({
     marginRight: 5,
     marginTop: 10,
     // backgroundColor: 'white'
-},
+  },
 });
