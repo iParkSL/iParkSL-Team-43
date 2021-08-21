b94a82d69db98a5de74ad472925b7039af512df0
RajapakshaSN <shasikalanimanthi@gmail.com>
2021-08-21 20:19:38 +0530

    Created book park route file

---
+++ CustomerApp/Server/routes/bookPark.js
@@ -0,0 +1,27 @@
+const express =require('express');
+const router = express.Router();
+const db=require("../config/db");
+
+
+router.post('/',function(req, res){
+    const vehicleNo=req.body.vehicleNo; 
+    const vehicleType=req.body.vehicleType; 
+    const paymentMethod=req.body.paymentMethod; 
+    const QrCode=req.body.QrCode; 
+    const CustomerID=req.body.CustomerID; 
+    const ParkID=req.body.ParkID; 
+    const EstimatedDuration=req.body.EstimatedDuration; 
+
+
+    
+
+    const bookPark = `insert into booking(vehicleNo,vehicleType,paymentMethod,QrCode,CustomerID,ParkID) values(?,?,?,?,?,?)`;
+
+    db.query(bookPark,[vehicleNo,vehicleType,paymentMethod,QrCode,CustomerID,ParkID,EstimatedDuration,date,Time],(err,result)=>{
+        res.json("SUCCESS");
+    });
+
+} );
+
+
+module.exports=router;
