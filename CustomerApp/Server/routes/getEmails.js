9483e338ca273be4115b742c9ce804432484bc3b
TRC98 <trchamod98@gmail.com>
2021-09-14 11:33:11 +0530

    Create backend routes for customer resetPassword
    Create checkCode js, forgetPassword js, getEmail js, resetPassword js

---
+++ CustomerApp/Server/routes/getEmails.js
@@ -0,0 +1,25 @@
+const express =require('express');
+const router = express.Router();
+const db=require("../config/db");
+
+
+
+router.get("/",(req,res)=>{
+    
+     var id=req.query.id;
+     console.log(id);
+     var query = 'SELECT email FROM customers';
+     console.log(query);
+ 
+     db.query(query,function(error, rows,fields){
+         if(error)console.log(error);
+ 
+         else{
+             console.log(rows);
+             res.send(rows);
+         }
+     });
+     
+ });
+
+module.exports=router;   
